52fa76cd4cfbe0c783e0b6c294d290fc1b6de9e6
Anders Tolborg <anderstolborg@sybogames.com>
2016-10-05 16:08:30 +0200

    first commit

---
+++ Assets/EventManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventManager : MonoBehaviour 
+{
+	public static event System.Action OnShowPanel;
+
+
+	public void ShowPanel()
+	{
+		if (OnShowPanel != null)
+		{
+			OnShowPanel();
+		}
+	}
+
+
+}
---
+++ Assets/PanelController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PanelController : MonoBehaviour 
+{
+	private Animator anim;
+	private bool isShown = false;
+
+
+	void Start()
+	{
+		anim = transform.GetComponent<Animator>();
+	}
+
+
+
+
+	void OnEnable()
+	{
+		EventManager.OnShowPanel += ShowPanel;
+	}
+
+
+	void OnDisable()
+	{
+		EventManager.OnShowPanel -= ShowPanel;
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	void ShowPanel()
+	{
+		if (!isShown) 
+		{
+			anim.Play("PanelSlideIn");
+			isShown = true;
+		}
+		else
+		{
+			anim.Play("PanelSlideOut");
+			isShown = false;
+		}
+
+	}
+
+}
