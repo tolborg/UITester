52d49d4eca31acd216146de4a3f6691bf1aab5e5
Anders Tolborg <anders@tolborg.dk>
2016-10-08 13:02:13 +0200

    Animate panel

---
+++ Assets/Panel/PanelController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PanelController : MonoBehaviour 
+{
+
+	Animator animator;
+
+
+	void Start()
+	{
+		animator = GetComponent<Animator>();
+	}
+
+
+
+
+
+
+	public void TogglePanel()
+	{
+
+		if (animator.GetBool("isActive"))
+		{
+			animator.SetBool("isActive", false);
+		}
+		else
+		{
+			animator.SetBool("isActive", true);
+		}
+			
+	}
+
+
+}
